4739fc160d0a31c3e9f41b1ff8c342f2aed64732
Mateusz Klosek <klosekmateusz@gmail.com>
2022-02-26 19:49:54 +0100

    Section 4

--- src/com/mateusz/controller/LoginWindowController.java
+++ src/com/mateusz/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.mateusz.controller;
 
 import com.mateusz.EmailManger;
+import com.mateusz.controller.services.LoginService;
+import com.mateusz.model.EmailAccount;
 import com.mateusz.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -25,11 +27,37 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+        boolean filedAreValid;
+        if (filedAreValid()) {
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(),
+                    passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManger);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull!!!" + emailAccount);
+            }
+        }
+
         System.out.println("clicked button!");
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
 
 
+    }
+
+    private boolean filedAreValid() {
+        if (emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill eamil");
+            return false;
+        }
+        if (passwordField.getText().isEmpty()) {
+            errorLabel.setText("Please fill eamil");
+            return false;
+        }
+
+        return true;
     }
 }
